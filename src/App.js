455ef76970c6f1335859cf1437d61b302090bdf7
Scott Simpson <>
2018-04-29 09:25:27 -0700

    added defaultFunctionArguments

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import NewVariables from "./components/NewVariables";
 import ArrowFunctions from "./components/ArrowFunctions";
 import TheThisKeyword from "./components/TheThisKeyword";
 import SelectingElements from "./components/SelectingElements";
+import DefaultFunctionArguments from "./components/DefaultFunctionArguments";
 
 class App extends Component {
-  render() {
-    return (
-        <div className="app">
-          <SelectingElements/>
-          <NewVariables />
-          <ArrowFunctions />
-          <TheThisKeyword/>
-        </div>
+    render() {
+        return (
+            <div className="app">
+                <SelectingElements/>
+                <NewVariables/>
+                <ArrowFunctions/>
+                <TheThisKeyword/>
+                <DefaultFunctionArguments/>
+            </div>
 
-    );
-  }
+        );
+    }
 }
 
 export default App;
