446d05dc833d025ff659b991384dbffb678dea1f
Scott Simpson <>
2018-04-29 15:02:00 -0700

    ready for module #3, template strings

---
+++ src/components/ArrowFunctionsWhenNotToUse.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+export default class ArrowFunctionsWhenNotToUse extends Component {
+
+    render() {
+        return (
+            <div className="items">
+                <h1>Arrow Functions — When not to use</h1>
+                <p>There are some cases when an arrow function not the best choice</p>
+                <ol>
+                    <li>When you have a `click event` handler attached to a button, and need the click to target the
+                        button you click.</li>
+                    <li>When you need a method to bind to an object</li>
+                    <li>When you need to add a prototype method</li>
+                    <li>When you need the arguments object</li>
+                </ol>
+                <iframe height='265' scrolling='no' title='Arrow functions'
+                        src='//codepen.io/simply-simpy/embed/eac01e99a2002a55c7d2615dde044b81/?height=265&theme-id=0&default-tab=js,result&embed-version=2&editable=true'
+                        frameBorder='no' allowTransparency='true' allowFullscreen='true'
+                        style={{width: 100 + "%", height: 800 + "px"}}>See the Pen <a
+                    href='https://codepen.io/simply-simpy/pen/eac01e99a2002a55c7d2615dde044b81/'>Arrow functions</a> by
+                    simply-simpy (<a href='https://codepen.io/simply-simpy'>@simply-simpy</a>) on <a
+                        href='https://codepen.io'>CodePen</a>.
+                </iframe>
+            </div>
+        );
+    }
+}
+
--- src/components/DefaultFunctionArguments.js
+++ src/components/DefaultFunctionArguments.js
@@ -1,29 +1,22 @@
 import React, {Component} from 'react';
 
-
 export default class DefaultFunctionArguments extends Component {
-
-  render() {
-    return (
-        <div className="items">
-          <h1>Default Function Arguments</h1>
-          <p>Three main benefits</p>
-          <ol>
-            <li>Much more concise</li>
-            <li>Implicit returns</li>
-            <li>Doesn't rebind the value of <code>this</code> when you use the function inside another function (handy
-              for click events)
-            </li>
-          </ol>
-          <iframe height='265' scrolling='no' title='Arrow functions'
-                  src='//codepen.io/simply-simpy/embed/eac01e99a2002a55c7d2615dde044b81/?height=265&theme-id=0&default-tab=js,result&embed-version=2&editable=true'
-                  frameBorder='no' allowTransparency='true' allowFullscreen='true' style={{width: 100 + "%", height: 800 + "px"}}>See the Pen <a
-              href='https://codepen.io/simply-simpy/pen/eac01e99a2002a55c7d2615dde044b81/'>Arrow functions</a> by
-            simply-simpy (<a href='https://codepen.io/simply-simpy'>@simply-simpy</a>) on <a
-                href='https://codepen.io'>CodePen</a>.
-          </iframe>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="items">
+                <h1>Default Function Arguments</h1>
+                <p>ES6 allows the setting of default arguments when the function is declared</p>
+                <p>These functions are overridable as well</p>
+                <iframe height='265' scrolling='no' title='Arrow functions'
+                        src='//codepen.io/simply-simpy/embed/LmxdeE/?height=265&theme-id=0&default-tab=js,result&embed-version=2&editable=true'
+                        frameBorder='no' allowTransparency='true' allowFullscreen='true'
+                        style={{width: 100 + "%", height: 800 + "px"}}>See the Pen <a
+                    href='https://codepen.io/simply-simpy/pen/eac01e99a2002a55c7d2615dde044b81/'>Arrow functions</a> by
+                    simply-simpy (<a href='https://codepen.io/simply-simpy'>@simply-simpy</a>) on <a
+                        href='https://codepen.io'>CodePen</a>.
+                </iframe>
+            </div>
+        );
+    }
 }
 
